b29ce4dc8fd9cb5b023b34cd8b7cf735a5e900ec
Audun Halland <audun.halland@pm.me>
2025-01-24 14:47:06 +0100

    refactor: greatly simplify connect client by using one tunnel with multiple http2 streams

--- lib/authly-connect/src/client.rs
+++ lib/authly-connect/src/client.rs
@@ -1,198 +1,113 @@
 use std::{
-    future::Future,
-    pin::Pin,
     sync::Arc,
     task::{Context, Poll},
 };
 
 use authly_common::proto::connect::authly_connect_client::AuthlyConnectClient;
 use axum::body::Bytes;
-use futures_util::{future::BoxFuture, ready, FutureExt};
+use futures_util::future::BoxFuture;
 use http::{
     uri::{Authority, Scheme},
     Uri,
 };
 use hyper::client::conn::http2::SendRequest;
-use hyper_util::rt::{TokioExecutor, TokioIo};
-use pin_project_lite::pin_project;
+use hyper_util::rt::{TokioExecutor, TokioIo, TokioTimer};
 use rustls::{pki_types::ServerName, ClientConfig};
-use tokio_rustls::{client::TlsStream, TlsConnector};
+use tokio_rustls::TlsConnector;
 use tokio_util::sync::CancellationToken;
-use tonic::{body::BoxBody, transport::Channel};
-use tower::{Service, ServiceExt};
-use tower_04::buffer::Buffer;
+use tonic::body::BoxBody;
+use tower::Service;
 use tracing::{info, trace};
 
 use crate::SERVER_NAME;
 
-use super::tunnel::{authly_connect_client_tunnel, ClientSideTunnel, StdError};
-
-const TUNNEL_BUFSIZE: usize = 128;
+use super::tunnel::{authly_connect_client_tunnel, StdError};
 
 /// Create a gRPC service (client-side) that tunnels through AuthlyConnect
 pub async fn new_authly_connect_grpc_client_service(
     connect_uri: Bytes,
     tls_client_config: Arc<ClientConfig>,
-    cancel: CancellationToken,
-) -> anyhow::Result<grpc_service::TunneledGrpcClientService> {
+    terminate: CancellationToken,
+) -> anyhow::Result<TunneledGrpcClientService> {
     let endpoint = tonic::transport::Endpoint::from_shared(connect_uri.clone()).unwrap();
     let channel = endpoint.connect().await?;
 
-    Ok(grpc_service::TunneledGrpcClientService {
-        make_send_request: MakeTunneledSendRequestService {
-            tunnel_buffer: Buffer::new(
-                TunneledTlsStreamService {
-                    connect_uri,
-                    channel,
-                    tls_client_config,
-                    cancel: cancel.clone(),
-                },
-                TUNNEL_BUFSIZE,
-            ),
-            cancel,
-        },
+    let close_signal = terminate.child_token();
+
+    let raw_tunnel =
+        authly_connect_client_tunnel(AuthlyConnectClient::new(channel.clone()), terminate.clone())
+            .await?;
+
+    let tls_tunnel = {
+        let connector = TlsConnector::from(tls_client_config.clone());
+        let server_name = ServerName::try_from(SERVER_NAME).unwrap();
+        connector.connect(server_name, raw_tunnel).await?
+    };
+
+    let http2_builder = hyper::client::conn::http2::Builder::new(TokioExecutor::new())
+        .timer(TokioTimer::new())
+        .clone();
+
+    let (send_request, connection) = http2_builder
+        .handshake::<_, BoxBody>(TokioIo::new(tls_tunnel))
+        .await?;
+
+    tokio::spawn({
+        let close_signal = close_signal.clone();
+        async move {
+            tokio::select! {
+                result = connection => {
+                    if let Err(err) = result {
+                        info!(?err, "Tunneled GRPC client connection error");
+                    }
+                }
+                _ = close_signal.cancelled() => {
+                    info!("Connect connection explicitly closed");
+                }
+            }
+        }
+    });
+
+    Ok(TunneledGrpcClientService {
+        send_request_buffer: tower_04::buffer::Buffer::new(
+            SendTunneledRequestService { send_request },
+            1024,
+        ),
+        close_signal,
     })
 }
 
-mod grpc_service {
-    use super::*;
+pub struct TunneledGrpcClientService {
+    send_request_buffer:
+        tower_04::buffer::Buffer<SendTunneledRequestService, http::Request<BoxBody>>,
 
-    pub struct TunneledGrpcClientService {
-        pub(super) make_send_request: MakeTunneledSendRequestService,
-    }
-
-    impl Service<http::Request<BoxBody>> for TunneledGrpcClientService {
-        type Response = http::Response<BoxBody>;
-        type Error = StdError;
-        type Future = ResponseFuture;
-
-        fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-            trace!("TunneledGrpcClientService::poll_ready");
-            Service::poll_ready(&mut self.make_send_request, cx)
-        }
-
-        fn call(&mut self, req: http::Request<BoxBody>) -> Self::Future {
-            trace!("TunneledGrpcClientService::call");
-            let future = self.make_send_request.call(());
-
-            ResponseFuture {
-                state: State::MakeSendRequest { future },
-                req: Some(req),
-            }
-        }
-    }
-
-    pin_project! {
-        pub struct ResponseFuture {
-            #[pin]
-            state: State,
-
-            req: Option<http::Request<BoxBody>>,
-        }
-    }
-
-    pin_project! {
-        #[project = StateProj]
-        enum State {
-            MakeSendRequest {
-                #[pin]
-                future: <MakeTunneledSendRequestService as Service<()>>::Future,
-            },
-            SendRequestPollReady {
-                svc: SendTunneledRequestService,
-            },
-            SendRequest {
-                #[pin]
-                future: <SendTunneledRequestService as Service<http::Request<BoxBody>>>::Future,
-            }
-        }
-    }
-
-    impl Future for ResponseFuture {
-        type Output = Result<http::Response<BoxBody>, StdError>;
-
-        fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            let mut zelf = self.project();
-
-            loop {
-                match zelf.state.as_mut().project() {
-                    StateProj::MakeSendRequest { future } => {
-                        trace!("grpc::ResponseFuture::MakeSendRequest");
-                        let send_request = ready!(future.poll(cx))?;
-
-                        zelf.state
-                            .set(State::SendRequestPollReady { svc: send_request });
-                    }
-                    StateProj::SendRequestPollReady { svc } => {
-                        trace!("grpc::ResponseFuture::SendRequestPollReady");
-                        ready!(svc.poll_ready(cx))?;
-                        let future = svc.call(zelf.req.take().expect("request already sent"));
-                        zelf.state.set(State::SendRequest { future });
-                    }
-                    StateProj::SendRequest { future } => {
-                        trace!("grpc::ResponseFuture::SendRequest");
-                        let response = ready!(future.poll(cx))?;
-                        return Poll::Ready(Ok(response));
-                    }
-                }
-            }
-        }
-    }
+    close_signal: CancellationToken,
 }
 
-#[derive(Clone)]
-struct MakeTunneledSendRequestService {
-    tunnel_buffer: tower_04::buffer::Buffer<TunneledTlsStreamService, ()>,
-    cancel: CancellationToken,
-}
-
-impl Service<()> for MakeTunneledSendRequestService {
-    type Response = SendTunneledRequestService;
+impl Service<http::Request<BoxBody>> for TunneledGrpcClientService {
+    type Response = http::Response<BoxBody>;
     type Error = StdError;
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+    type Future = tower_04::buffer::future::ResponseFuture<
+        <SendTunneledRequestService as Service<http::Request<BoxBody>>>::Future,
+    >;
 
-    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Service::poll_ready(&mut self.send_request_buffer, cx)
     }
 
-    fn call(&mut self, _req: ()) -> Self::Future {
-        let mut tunnel_buffer = self.tunnel_buffer.clone();
-        let cancel = self.cancel.clone();
-
-        async move {
-            // TODO: Should reuse tunnels?
-            tunnel_buffer.ready().await?;
-            let available_tunnel = tunnel_buffer.call(()).await?;
-
-            let builder = hyper::client::conn::http2::Builder::new(TokioExecutor::new())
-                // .initial_stream_window_size(endpoint.init_stream_window_size)
-                // .initial_connection_window_size(endpoint.init_connection_window_size)
-                // .keep_alive_interval(endpoint.http2_keep_alive_interval)
-                // .timer(TokioTimer::new())
-                .clone();
-
-            let (send_request, connection) = builder
-                .handshake::<_, BoxBody>(TokioIo::new(available_tunnel))
-                .await?;
-
-            tokio::spawn(async move {
-                tokio::select! {
-                    result = connection => {
-                        if let Err(err) = result {
-                            info!(?err, "Tunneled GRPC client connection error");
-                        }
-                    }
-                    _ = cancel.cancelled() => {}
-                }
-            });
-
-            Ok(SendTunneledRequestService { send_request })
-        }
-        .boxed()
+    fn call(&mut self, req: http::Request<BoxBody>) -> Self::Future {
+        self.send_request_buffer.call(req)
     }
 }
 
-struct SendTunneledRequestService {
+impl Drop for TunneledGrpcClientService {
+    fn drop(&mut self) {
+        // When the service is dropped, end the connection manager task
+        self.close_signal.cancel();
+    }
+}
+
+pub struct SendTunneledRequestService {
     send_request: SendRequest<BoxBody>,
 }
 
@@ -225,47 +140,5 @@ impl Service<http::Request<BoxBody>> for SendTunneledRequestService {
                 .map_err(Into::into)
                 .map(|res| res.map(tonic::body::boxed))
         })
-    }
-}
-
-/// Service that produces a new client side tunnel
-#[derive(Clone)]
-struct TunneledTlsStreamService {
-    connect_uri: Bytes,
-    channel: Channel,
-    tls_client_config: Arc<ClientConfig>,
-    cancel: CancellationToken,
-}
-
-impl Service<()> for TunneledTlsStreamService {
-    type Response = TlsStream<ClientSideTunnel>;
-    type Error = StdError;
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-
-    fn poll_ready(&mut self, cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
-        trace!("TunneledTlsStreamService::poll_ready");
-        Service::poll_ready(&mut self.channel, cx).map_err(|err| err.into())
-    }
-
-    fn call(&mut self, _: ()) -> Self::Future {
-        info!(
-            "opening new tunnel to {:?}",
-            std::str::from_utf8(&self.connect_uri)
-        );
-
-        let channel = self.channel.clone();
-        let tls_client_config = self.tls_client_config.clone();
-        let cancel = self.cancel.clone();
-
-        async move {
-            let raw_tunnel =
-                authly_connect_client_tunnel(AuthlyConnectClient::new(channel), cancel.clone())
-                    .await?;
-
-            let connector = TlsConnector::from(tls_client_config);
-            let server_name = ServerName::try_from(SERVER_NAME).unwrap();
-            Ok(connector.connect(server_name, raw_tunnel).await?)
-        }
-        .boxed()
     }
 }
