7d3efb1e21b63e1998ffcd7321933fc82b1a93de
Audun Halland <audun.halland@pm.me>
2025-01-30 21:25:49 +0100

    fix: don't rebuild on change in static/vendor
    This makes the build script rerun each time, since running it re-downloads vendor content
    which updates modified-time.

--- lib/authly-webstatic/build.rs
+++ lib/authly-webstatic/build.rs
@@ -1,6 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=update.sh");
-    println!("cargo:rerun-if-changed=static/vendor");
 
     std::process::Command::new("sh")
         .arg("update.sh")
